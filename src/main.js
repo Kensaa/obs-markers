13e546308326fe898cdb1fbe3b4fb29eaa37aef8
Kensaa <kensadevs@gmail.com>
2022-09-17 20:57:48 +0200

    shifted markers 30sec earlier

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import * as path from 'path'
 const obs = new OBSWebSocket()
 let output = []
 
-
 if (!fs.existsSync('config.json')) {
     const defaultConfig = {
         "address": "ws://localhost:4455",
@@ -19,12 +18,39 @@ const config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
 
 obs.connect(config.address,config.password)
 
+function parseTimecode(timecode){
+    //0:15:05.0083
+    const split = timecode.split(':')
+    return {
+        hours: parseInt(split[0]),
+        minutes: parseInt(split[1]),
+        seconds: parseInt(split[2].split('.')[0]),
+        milliseconds: parseInt(split[2].split('.')[1])
+    }
+}
+
 uIOhook.on('keydown', async e => {
     if (e.keycode === UiohookKey.F7) {
         console.log('adding marker')
         const res = await obs.call('GetRecordStatus')
+        const time = parseTimecode(res.outputTimecode)
+        //remove 30seconds from the econds
+        time.seconds -= 30
+        if (time.seconds < 0) {
+            time.seconds = 60 + time.seconds
+            time.minutes -= 1
+        }
+        if (time.minutes < 0) {
+            time.minutes = 60 + time.minutes
+            time.hours -= 1
+        }
+        if(time.hours < 0){
+            time.hours = 0
+            time.minutes = 0
+            time.seconds = 0
+        }
         output.push({
-            timecode: res.outputTimecode,
+            timecode: `${time.hours}:${time.minutes}:${time.seconds}.${time.milliseconds}`,
             duration: res.outputDuration
         })
     }
