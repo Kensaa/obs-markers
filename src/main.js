9f39a489bebcfe8ad62744c82f9a5e4afe4e97dd
Kensaa <kensadevs@gmail.com>
2022-09-17 16:08:22 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,46 @@
+import { uIOhook, UiohookKey } from 'uiohook-napi'
+import OBSWebSocket from 'obs-websocket-js'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const obs = new OBSWebSocket()
+let output = []
+
+let config = JSON.parse(fs.readFileSync('config.json', 'utf8'))
+
+if (!config.address || !config.password) {
+    const defaultConfig = {
+        "address": "ws://localhost:4455",
+        "password": "password"
+    }
+    fs.writeFileSync('config.json', JSON.stringify(defaultConfig, null, 4))
+    config = defaultConfig
+}
+
+obs.connect(config.address,config.password)
+
+uIOhook.on('keydown', async e => {
+    if (e.keycode === UiohookKey.F7) {
+        console.log('adding marker')
+        const res = await obs.call('GetRecordStatus')
+        output.push({
+            timecode: res.outputTimecode,
+            duration: res.outputDuration
+        })
+    }
+})
+
+obs.on('RecordStateChanged',(res)=>{
+    if(res.outputState === 'OBS_WEBSOCKET_OUTPUT_STOPPED'){
+        const dir = path.dirname(res.outputPath)
+        const filename = path.basename(res.outputPath, '.mkv')
+        
+        const file = path.join(dir, `${filename} markers.json`)
+        fs.writeFileSync(file, JSON.stringify(output, null, 4))
+        console.log('saved markers to', file)
+
+        output = []
+    }
+})
+
+uIOhook.start()
